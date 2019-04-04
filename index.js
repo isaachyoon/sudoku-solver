07591117a320981d6d192617e49e65591250c11d
Isaac Yoon <isaac.yoon@aofl.com>
2019-04-03 21:10:03 -0700

    update with logic

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 
-const matrix = [];
-for (let i = 0; i < 9; i++) {
-  const columnArr = new Array(9).fill(0);
-  matrix.push(columnArr);
-}
-console.log(matrix);
+// const matrix = [];
+// for (let i = 0; i < 9; i++) {
+//   const columnArr = new Array(9).fill(0);
+//   matrix.push(columnArr);
+// }
+// console.log(matrix);
+
+const matrix = [
+  [0, 0, 1, 9, 8, 4, 7, 6, 0],
+  [6, 0, 0, 0, 5, 7, 0, 0, 0],
+  [8, 0, 7, 0, 1, 0, 0, 0, 0],
+  [9, 6, 0, 3, 0, 8, 1, 0, 5],
+  [1, 8, 5, 0, 2, 0, 0, 7, 3],
+  [3, 0, 0, 0, 0, 0, 2, 0, 8],
+  [2, 1, 0, 0, 0, 0, 0, 3, 6],
+  [0, 0, 0, 1, 0, 0, 0, 0, 4],
+  [0, 9, 6, 0, 0, 2, 5, 1, 0]
+]
 
 
 
@@ -38,14 +50,30 @@ updateMatrix = (e) => {
   matrix[row][col] = value;
 }
 
-solveSudoku = () => {
+solveSudoku = (row, col) => {
 
+  if (col === 9) {
+    col = 0;
+    row++;
+  }
+  if (row === 9 && col === 9) {
+    return matrix;
+  }
+  if (matrix[row][col] === 0) {
+    let options = getOptions(row, col);
+    for (let i = 0; i < options.length; i++) {
+      matrix[row][col] = options[i];
+      if (checkRow(row) && checkCol(col) && checkThreeByThree(row, col)) {
+        solveSudoku(row, col+1);
+      }
+    }
+  }
 }
 
 const addEventListener = (table) => {
   table.addEventListener('input', updateMatrix);
   const button = document.querySelector('#solve');
-  button.addEventListener('click', solveSudoku);
+  button.addEventListener('click', () => solveSudoku(0,0));
 }
 
 const init = () => {
@@ -55,6 +83,11 @@ const init = () => {
 }
 
 init();
+
+function getOptions(row, col) { //iterate through column, rows, and 3 x 3 and generate a list of options
+
+}
+
 
 function checkRow(row) {
   const set = new Set();
@@ -80,6 +113,26 @@ function checkCol(col) {
   return true;
 }
 
-function checkThreeByThree() {
+function checkThreeByThree(row, col) {
+  const set = new Set();
+  let r = findIndex(row);
+  let c = findIndex(col);
+  console.log(r, c)
+  for (let i = r; i < r+3; i++) {
+    for (let j= c; j < c+3; j++) {
+      if (parseInt(matrix[i][j]) > 0 && !set.has(matrix[i][j])) {
+        set.add(matrix[i][j]);
+      } else if (set.has(matrix[i][j])) {
+        console.log(set)
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
+function findIndex(num) {
+  if (num < 3) return 0;
+  else if (num < 6) return 3;
+  else if (num < 9) return 5;
 }
